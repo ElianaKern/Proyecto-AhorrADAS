02f53c1da55e2e770b3580a699e96a58e476a1fe
Eliana Kern <Eliana Kern>
2021-10-28 16:16:50 -0300

    agrego cambios del nav y funciones

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hola, soy js funcionando")
+const seccionPrincipal = document.getElementById("seccion-principal")
+const seccionCategorias = document.getElementById("seccion-categorias")
+const seccionReportes = document.getElementById("seccion-reportes-vacio")
+const linkSeccionPrincipalBalance = document.getElementById('link-balance')
+const linkCategorias = document.getElementById('link-categorias')
+const linkReportes = document.getElementById("link-reportes")
+
+linkCategorias.onclick = () => {
+    seccionCategorias.classList.remove('is-hidden');
+    seccionPrincipal.classList.add("is-hidden")
+    seccionReportes.classList.add("is-hidden")
+}
+
+linkSeccionPrincipalBalance.onclick = () => {
+    seccionPrincipal.classList.remove("is-hidden")
+    seccionReportes.classList.add("is-hidden")
+    seccionCategorias.classList.add("is-hidden")
+}
+
+linkReportes.onclick = () => {
+    seccionReportes.classList.remove("is-hidden")
+    seccionPrincipal.classList.add("is-hidden")
+    seccionCategorias.classList.add("is-hidden")
+}
